d1a85c98ef8663cc140ab37958344ea1699848db
HaiderCoding-1 <siriisaslut@gmail.com>
2025-06-20 12:41:03 -0700

    Add files via upload

--- Core.cs
+++ Core.cs
@@ -1,24 +1,28 @@
 ﻿using MelonLoader;
 using UnityEngine;
 using System.Reflection;
+using System;
 
 [assembly: MelonInfo(typeof(BananaShooterMod.Core), "Banana Shooter IMGUI Base", "1.0.0", "Haider x Moonlight", null)]
-
 [assembly: MelonGame("Daniel", "Banana Shooter")]
 
 namespace BananaShooterMod
 {
     public class Core : MelonMod
     {
-        // --- IMGUI Settings ---
+        // IMGUI Settings
         private bool _showMenu = true;
-        private Rect _windowRect = new Rect(20, 20, 250, 200); 
+        private Rect _windowRect = new Rect(100, 190, 350, 400);
+        private GUISkin _customSkin;
+        private Jump _jumpHandler;
 
-     
         public override void OnInitializeMelon()
         {
             LoggerInstance.Msg("IMGUI Mod Initialized! Press F4 to toggle the menu.");
+            SetupGUISkin();
+            _jumpHandler = new Jump();
         }
+
         public override void OnUpdate()
         {
             if (Input.GetKeyDown(KeyCode.F4))
@@ -27,31 +31,99 @@ namespace BananaShooterMod
             }
         }
 
-        
         public override void OnGUI()
         {
             if (!_showMenu)
             {
                 return;
             }
-
-            GUI.skin = GUI.skin;
-
+            GUI.skin = _customSkin;
             _windowRect = GUI.Window(0, _windowRect, (GUI.WindowFunction)MyWindow, "Banana Mod Menu");
         }
 
-        /// <param name="windowID"></param>
         private void MyWindow(int windowID)
         {
-            // --- Window Content Goes Here ---
+            float yPos = 40f;
+            float width = _windowRect.width - 40f;
+            float height = 30f;
+            float padding = 10f;
+            float xPos = 20f;
 
-            GUI.Label(new Rect(10, 20, 230, 20), "Welcome to the Mod Menu!");
+            GUI.Label(new Rect(xPos, yPos, width, height), "Welcome to the Mod Menu!", _customSkin.label);
+            yPos += height + padding;
 
-            if (GUI.Button(new Rect(10, 50, 230, 30), "Click Me!"))
+            // Jump Value
+            if (GUI.Button(new Rect(xPos, yPos, width, height), "Set Max Jump"))
             {
-                LoggerInstance.Msg("Button was clicked!");
+                LoggerInstance.Msg("Set Max Jump button clicked!");
+                _jumpHandler.GetAndSetMaxJump();
+            }
+            yPos += height + padding;
+
+            // Dumper Tool 
+            if (GUI.Button(new Rect(xPos, yPos, width, height), "Dump Player Info"))
+            {
+                LoggerInstance.Msg("Dump Player Info button clicked!");
+                _jumpHandler.DumpPlayerMovementInfo();
+            }
+            yPos += height + padding;
+
+            if (GUI.Button(new Rect(xPos, yPos, width, height), "Another Option"))
+            {
+                LoggerInstance.Msg("Another option was selected!");
+            }
+            yPos += height + padding;
+
+            float closeButtonY = _windowRect.height - height - 20f;
+            if (GUI.Button(new Rect(xPos, closeButtonY, width, height), "Close Menu"))
+            {
+                _showMenu = false;
             }
             GUI.DragWindow();
+        }
+
+        private void SetupGUISkin()
+        {
+            _customSkin = ScriptableObject.CreateInstance<GUISkin>();
+            var darkGray = new Color(0.12f, 0.12f, 0.12f, 0.98f);
+            var midGray = new Color(0.2f, 0.2f, 0.2f, 1f);
+            var lightGray = new Color(0.3f, 0.3f, 0.3f, 1f);
+            var textColor = Color.white;
+            var highlightColor = new Color(1f, 0.8f, 0.4f);
+            var windowBg = MakeTex(1, 1, darkGray);
+            var buttonBg = MakeTex(1, 1, midGray);
+            var buttonHoverBg = MakeTex(1, 1, lightGray);
+            _customSkin.window.normal.background = windowBg;
+            _customSkin.window.normal.textColor = textColor;
+            _customSkin.window.padding = new RectOffset(15, 15, 25, 15);
+            _customSkin.window.alignment = TextAnchor.UpperCenter;
+            _customSkin.window.fontSize = 16;
+            _customSkin.button.normal.background = buttonBg;
+            _customSkin.button.normal.textColor = textColor;
+            _customSkin.button.hover.background = buttonHoverBg;
+            _customSkin.button.hover.textColor = highlightColor;
+            _customSkin.button.active.background = buttonHoverBg;
+            _customSkin.button.padding = new RectOffset(10, 10, 10, 10);
+            _customSkin.button.margin = new RectOffset(0, 0, 5, 5);
+            _customSkin.button.fontSize = 14;
+            _customSkin.label.normal.textColor = textColor;
+            _customSkin.label.alignment = TextAnchor.MiddleCenter;
+            _customSkin.label.fontSize = 16;
+        }
+
+        private Texture2D MakeTex(int width, int height, Color col)
+        {
+            var pix = new Color[width * height];
+            for (int i = 0; i < pix.Length; i++)
+            {
+                pix[i] = col;
+            }
+            var result = new Texture2D(width, height);
+            result.SetPixels(pix);
+            result.Apply();
+            UnityEngine.Object.DontDestroyOnLoad(result);
+            result.hideFlags = HideFlags.HideAndDontSave;
+            return result;
         }
     }
 }
---
+++ Jump.cs
@@ -0,0 +1,85 @@
+﻿using MelonLoader;
+using UnityEngine;
+using System.Reflection;
+using System;
+using Il2CppMovement;
+
+namespace BananaShooterMod
+{
+    public class Jump
+    {
+        public void GetAndSetMaxJump()
+        {
+            try
+            {
+                Type playerMovementType = typeof(PlayerMovement);
+
+                // --- THE FIX ---
+                // We are now using GetProperty instead of GetField.
+                PropertyInfo jumpFactorProperty = playerMovementType.GetProperty("jumpFactor",
+                    BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+
+                if (jumpFactorProperty == null)
+                {
+                    // This error shouldn't happen now, but it's good practice to keep it.
+                    MelonLogger.Error("Could not find the 'jumpFactor' property in PlayerMovement!");
+                    return;
+                }
+
+                PlayerMovement playerInstance = UnityEngine.Object.FindObjectOfType<PlayerMovement>();
+                if (playerInstance == null)
+                {
+                    MelonLogger.Error("Could not find a live instance of PlayerMovement!");
+                    return;
+                }
+
+                // Get the current value from the Property
+                float currentValue = (float)jumpFactorProperty.GetValue(playerInstance);
+                MelonLogger.Msg($"Current jump factor is: {currentValue}");
+
+                // Set the new value on the Property
+                jumpFactorProperty.SetValue(playerInstance, 20f);
+                MelonLogger.Msg("Jump factor set to 20!");
+            }
+            catch (Exception e)
+            {
+                MelonLogger.Error($"An error occurred during reflection: {e.Message}");
+            }
+        }
+    
+
+        public void DumpPlayerMovementInfo()
+        {
+            try
+            {
+                MelonLogger.Msg("==========================================");
+                MelonLogger.Msg("Dumping members of PlayerMovement class...");
+
+                Type playerMovementType = typeof(PlayerMovement);
+                var flags = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static;
+
+                // Get all Fields
+                FieldInfo[] fields = playerMovementType.GetFields(flags);
+                MelonLogger.Msg($"--- Fields ({fields.Length}) ---");
+                foreach (FieldInfo field in fields)
+                {
+                    MelonLogger.Msg($"Found Field: {field.Name} (Type: {field.FieldType.Name})");
+                }
+
+                // Get all Properties
+                PropertyInfo[] properties = playerMovementType.GetProperties(flags);
+                MelonLogger.Msg($"--- Properties ({properties.Length}) ---");
+                foreach (PropertyInfo property in properties)
+                {
+                    MelonLogger.Msg($"Found Property: {property.Name} (Type: {property.PropertyType.Name})");
+                }
+
+                MelonLogger.Msg("==========================================");
+            }
+            catch (Exception e)
+            {
+                MelonLogger.Error($"An error occurred during dumper: {e.Message}");
+            }
+        }
+    }
+}
